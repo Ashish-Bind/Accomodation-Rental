3d97ba5362855e0588d85a6c1e16215b80e53974
kumar159 <kumarmadagunaki145@gmail.com>
2023-09-11 21:59:10 +0530

    Added some code in account page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,8 +18,10 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/account" element={<AccountPage />} />
-          <Route path="/account/:subpage" element={<AccountPage />} />
+          {/* <Route path="/account" element={<AccountPage />} /> */}
+          {/* <Route path="/account/bookings" element={<AccountPage />} />
+          <Route path="/account/places" element={<AccountPage />} /> */}
+          <Route path="/account/:subpage?" element={<AccountPage />} />
         </Route>
       </Routes>
     </UserProvider>
--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -3,13 +3,42 @@ import { useUser } from '../context/UserContext'
 
 function AccountPage() {
   const { ready, user } = useUser()
+  let {subpage} = useParams();
+  if (subpage === undefined) {
+    subpage = 'profile';
+  }
+
   if (!ready) {
     return <div>Loading ...</div>
   }
   if (ready && !user) {
     return <Navigate to="/login" />
   }
-  return <div>Account Page for {user?.name}</div>
+
+  
+  function LinkClasses (type=null) {
+    let classes = 'py-2 px-6';
+    if (type === subpage) {
+      classes += 'bg-primary text-white rounded-full'
+    }
+    return classes;
+  }
+
+  return (
+    <div>
+      <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
+      <Link className={LinkClasses('profile')} to={'/account'}>My profile</Link>
+        <Link className={LinkClasses('bookings')} to={'/account/bookings'}>My bookings</Link>
+        <Link className={LinkClasses('places')} to={'/account/places'}>My accommodations</Link>
+      </nav>
+      {subpage === 'profile' && (
+        <div className="text-center max-w-lg mx-auto">
+          Logged in as {user.name} ({user.email})<br />
+          <button className="primary max-w-sm mt-2">Logout</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default AccountPage
